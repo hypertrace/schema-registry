002c895fae97b624a4847b5ed22b757e62f65b51
Laxman Ch <60599147+laxmanchekka@users.noreply.github.com>
2022-09-16 11:02:49 +0530

    feat: schema registry version upgrade (5.5.1 to 7.2.1) (#21)

--- docker/build.gradle.kts
+++ docker/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.9.4"
-  id("org.hypertrace.docker-publish-plugin") version "0.9.4"
+  id("base")
+  id("org.hypertrace.docker-plugin")
+  id("org.hypertrace.docker-publish-plugin")
 }
 
 hypertraceDocker {
