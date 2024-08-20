580084fe6279e4a241afc45d13ce11a49063f09a
Laxman Ch <60599147+laxmanchekka@users.noreply.github.com>
2024-08-20 16:08:37 +0530

    gradle and gradle plugins upgrade (#42)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.9.9" apply false
-  id("org.hypertrace.docker-publish-plugin") version "0.9.9" apply false
+  id("org.hypertrace.docker-plugin") version "0.10.1" apply false
+  id("org.hypertrace.docker-publish-plugin") version "0.10.1" apply false
 }
