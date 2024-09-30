aa61f5a512b751110d0d13e2d41a132471f4d587
siddhant2001 <siddhant.jain@traceable.ai>
2024-09-30 19:48:58 +0530

    initialize cron job

---
+++ compatibility-changer/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+  id("base")
+  id("org.hypertrace.docker-plugin")
+  id("org.hypertrace.docker-publish-plugin")
+}
+
+hypertraceDocker {
+  defaultImage {
+    imageName.set("compatibility-changer")
+  }
+}
